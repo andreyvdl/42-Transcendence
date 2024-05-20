bd3a33cb75c67ea8411dbf2d5cb8ebd93e6409e6
adantas- <adantas-@student.42sp.org.br>
2024-05-20 18:13:27 -0300

    feat: basic pong game finished, need to fix colision

--- test.js
+++ test.js
@@ -1,56 +1,83 @@
 import * as THREE from 'three';
 import WebGL from 'three/addons/capabilities/WebGL.js'
 
+var keys = {};
+
+document.addEventListener("keydown", (event) => {
+		keys[event.key] = true;
+});
+
+document.addEventListener("keyup", (event) => {
+		keys[event.key] = false;
+});
+
+const WINDOW = {
+	width: 1024,
+	height: 768,
+	get aspect() { return this.width / this.height; },
+};
+
+const BALL_VELOCITY = {
+	x: 0.01,
+	z: 0.01,
+	timer: 0,
+	defaultX: 0.01,
+	defaultZ: 0.01,
+};
+
 const DEFAULT_Y = {
 	net: 0.5,
 	netTop: 0.73,
 	cylinder: 0.4,
-}
+};
+
+const SCORE = {
+	playerOne: 0,
+	playerTwo: 0,
+};
 
 if (WebGL.isWebGLAvailable()) {
 
 	const renderer = new THREE.WebGLRenderer();
 	const scene = new THREE.Scene();
-	// acho que 4:3 fica melhor que 16:9
-	const camera = new THREE.PerspectiveCamera(60, 1152 / 864, 0.1, 1000);
+	const camera = new THREE.PerspectiveCamera(80, WINDOW.aspect, 0.1, 1000);
 
 	setupRenderer(renderer);
 	document.querySelector('#pong_canvas').appendChild(renderer.domElement);
-	const ball = setupBall();
+
+	const ball3d = setupBall();
 	const ground = setupGround();
 	const light = [setupLight(-10, 10), setupLight(10, 10), setupLight(-10, -10), setupLight(10, -10)];
-	const player = [setupPlayer(-4, 0xff0000), setupPlayer(4, 0xffff00)];
-	const net = setupNet();
-	const cylinder = [setupCylinder(3.9), setupCylinder(-3.9)]
-	const netTop = setupNetTop();
+	let player = [setupPlayer(-4, 0xff0000), setupPlayer(4, 0xffff00)];
+	const canvas = [document.createElement('canvas'), document.createElement('canvas')];
+	const context = [canvas[0].getContext('2d'), canvas[1].getContext('2d')];
+
+	context[0].font = context[1].font = '48px Arial';
+	context[0].fillStyle = context[1].fillStyle = 'red';
+	context[0].fillText(SCORE.playerOne, 0, 50);
+	context[1].fillText(SCORE.playerTwo, 0, 50);
+
+	const texture = [new THREE.CanvasTexture(canvas[0]), new THREE.CanvasTexture(canvas[1])];
+	const spriteMaterial = [new THREE.SpriteMaterial({map: texture[0]}), new THREE.SpriteMaterial({map: texture[1]})];
+	const sprite = [new THREE.Sprite(spriteMaterial[0]), new THREE.Sprite(spriteMaterial[1])];
+
+	sprite[0].position.set(-0.1, 2, 4);
+	sprite[1].position.set(0.9, 2, 4);
+	scene.add(...sprite);
 
 	scene.background = new THREE.TextureLoader().load('./assets/xique-xique.jpg');
-	scene.add(ball, ground, net, netTop);
+	scene.add(ball3d, ground);
 	scene.add(...light);
 	scene.add(...player);
-	scene.add(...cylinder);
 
-	camera.position.set(0, 2.5, 5);
-	var dir = 0.05;
+	camera.position.set(0, 10, 0);
+	camera.lookAt(scene.position);
 	function animate() {
 		requestAnimationFrame(animate);
 
-		ball.rotation.x += 0.03;
-		ball.rotation.y -= 0.03;
-
-		const dist = Math.abs(ball.position.x);
-
-		if (dist > 4.0)
-			dir = -dir;
-		ball.position.x += dir;
-		camera.position.x = -ball.position.x;;
-		// fazer a rede descer ao invés de subir
-		cylinder[0].position.y = DEFAULT_Y.cylinder / (dist / 4);
-		cylinder[1].position.y = DEFAULT_Y.cylinder / (dist / 4);
-		netTop.position.y = DEFAULT_Y.netTop / (dist / 4);
-		net.position.y = DEFAULT_Y.net / (dist / 4);
+		ballMove(ball3d, player);
+		playerMove(player);
 		camera.lookAt(scene.position);
-		// camera.lookAt(ball.position);
 		renderer.render(scene, camera);
 	}
 	animate();
@@ -60,7 +87,7 @@ if (WebGL.isWebGLAvailable()) {
 }
 
 function setupRenderer(renderer) {
-	renderer.setSize(1152, 864);
+	renderer.setSize(WINDOW.width, WINDOW.height);
 	renderer.shadowMap.enabled = true;
 	renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 }
@@ -74,6 +101,7 @@ function setupBall() {
 	ball.position.y = 0.3;
 	ball.castShadow = true;
 	ball.receiveShadow = true;
+	ball.geometry.computeBoundingSphere();
 	return ball;
 }
 
@@ -106,34 +134,38 @@ function setupPlayer(x, color = 0x00ffff) {
 	return player;
 }
 
-function setupNet() {
-	const gridHelper = new THREE.GridHelper(1, 9, 0x00aa00, 0x00aa00);
-
-	gridHelper.castShadow = true;
-	gridHelper.rotateZ(-Math.PI / 2);
-	gridHelper.scale.set(0.5, 0, 7.9);
-	gridHelper.position.y = 0.5;
-	return gridHelper;
+function playerMove(player) {
+	if (keys["ArrowDown"])
+		player[1].position.z += 0.1;
+	if (keys["ArrowUp"])
+		player[1].position.z -= 0.1;
+	if (keys["s"])
+		player[0].position.z += 0.1;
+	if (keys["w"])
+		player[0].position.z -= 0.1;
 }
 
-function setupCylinder(z) {
-	const cyGeo = new THREE.CylinderGeometry(0.1, 0.1, 0.8);
-	const cyMat = new THREE.MeshStandardMaterial({color: 0x0000ff});
-	const cylinder = new THREE.Mesh(cyGeo, cyMat);
-
-	cylinder.position.set(0, 0.4, z);
-	cylinder.castShadow = true;
-	cylinder.receiveShadow = true;
-	return cylinder;
+function ballMove(ball3d, player) {
+	ball3d.position.x += BALL_VELOCITY.x;
+	ball3d.position.z += BALL_VELOCITY.z;
+	if (ball3d.position.z > 3.9 || ball3d.position.z < -3.9)
+		BALL_VELOCITY.z = -BALL_VELOCITY.z * (Math.abs(BALL_VELOCITY.z) < 1 ? 1.1 : 1);
+	else if (BALL_VELOCITY.timer > 0) {
+		BALL_VELOCITY.timer--;
+		return ;
+	}
+	else if (playerColision(ball3d, player)) {
+		BALL_VELOCITY.x = -BALL_VELOCITY.x * (Math.abs(BALL_VELOCITY.x) < 1 ? 1.1 : 1);
+		BALL_VELOCITY.timer = 500;
+	}
+	else if (ball3d.position.x > 4 || ball3d.position.x < -4)
+		BALL_VELOCITY.x = -BALL_VELOCITY.x * (Math.abs(BALL_VELOCITY.x) < 1 ? 1.1 : 1);
 }
 
-function setupNetTop() {
-	const netTopGeo = new THREE.BoxGeometry(0.1, 0.1, 7.6);
-	const netTopMat = new THREE.MeshStandardMaterial({color: 0xffffff});
-	const netTop = new THREE.Mesh(netTopGeo, netTopMat);
+function playerColision(ball3d, player) {
+	const sphere = new THREE.Sphere(ball3d.position, ball3d.geometry.boundingSphere.radius);
+	const box = new THREE.Box3().setFromObject(player[0]);
+	const box2 = new THREE.Box3().setFromObject(player[1]);
 
-	netTop.position.set(0, 0.73, 0);
-	netTop.castShadow = true;
-	netTop.receiveShadow = true;
-	return netTop;
+	return sphere.intersectsBox(box) || sphere.intersectsBox(box2);
 }
