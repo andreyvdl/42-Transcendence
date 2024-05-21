06ca84dcdedcd04ac3fadfedf13ae5773eefe3b5
adantas- <adantas-@student.42sp.org.br>
2024-05-21 17:48:06 -0300

    feat: score works and increased map size

--- test.js
+++ test.js
@@ -3,6 +3,8 @@ import WebGL from 'three/addons/capabilities/WebGL.js'
 
 var keys = {};
 
+var BALLPAUSE = false;
+
 document.addEventListener("keydown", (event) => {
 		keys[event.key] = true;
 });
@@ -12,23 +14,17 @@ document.addEventListener("keyup", (event) => {
 });
 
 const WINDOW = {
-	width: 1024,
-	height: 768,
+	width: window.innerWidth,
+	height: window.innerHeight,
 	get aspect() { return this.width / this.height; },
 };
 
 const BALL_VELOCITY = {
-	x: 0.01,
-	z: 0.01,
+	defaultX: 0.05,
+	defaultZ: 0.06,
+	x: 0.05,
+	z: 0.06,
 	timer: 0,
-	defaultX: 0.01,
-	defaultZ: 0.01,
-};
-
-const DEFAULT_Y = {
-	net: 0.5,
-	netTop: 0.73,
-	cylinder: 0.4,
 };
 
 const SCORE = {
@@ -37,7 +33,6 @@ const SCORE = {
 };
 
 if (WebGL.isWebGLAvailable()) {
-
 	const renderer = new THREE.WebGLRenderer();
 	const scene = new THREE.Scene();
 	const camera = new THREE.PerspectiveCamera(80, WINDOW.aspect, 0.1, 1000);
@@ -47,37 +42,55 @@ if (WebGL.isWebGLAvailable()) {
 
 	const ball3d = setupBall();
 	const ground = setupGround();
-	const light = [setupLight(-10, 10), setupLight(10, 10), setupLight(-10, -10), setupLight(10, -10)];
-	let player = [setupPlayer(-4, 0xff0000), setupPlayer(4, 0xffff00)];
+	const light = setupLight();
+	const player = [setupPlayer(-8, 0xff0000), setupPlayer(8, 0xffff00)];
 	const canvas = [document.createElement('canvas'), document.createElement('canvas')];
+
+	canvas[0].width = canvas[1].width = 512;
+	canvas[0].height = canvas[1].height = 512;
+
 	const context = [canvas[0].getContext('2d'), canvas[1].getContext('2d')];
 
-	context[0].font = context[1].font = '48px Arial';
+	context[0].font = context[1].font = '72px Arial';
 	context[0].fillStyle = context[1].fillStyle = 'red';
-	context[0].fillText(SCORE.playerOne, 0, 50);
-	context[1].fillText(SCORE.playerTwo, 0, 50);
+	context[0].fillText(SCORE.playerOne, 0, 256);
+	context[1].fillText(SCORE.playerTwo, 0, 256);
 
 	const texture = [new THREE.CanvasTexture(canvas[0]), new THREE.CanvasTexture(canvas[1])];
 	const spriteMaterial = [new THREE.SpriteMaterial({map: texture[0]}), new THREE.SpriteMaterial({map: texture[1]})];
-	const sprite = [new THREE.Sprite(spriteMaterial[0]), new THREE.Sprite(spriteMaterial[1])];
+	const sprites = [new THREE.Sprite(spriteMaterial[0]), new THREE.Sprite(spriteMaterial[1])];
 
-	sprite[0].position.set(-0.1, 2, 4);
-	sprite[1].position.set(0.9, 2, 4);
-	scene.add(...sprite);
-
+	sprites[0].position.set(0, 5, 4);
+	sprites[1].position.set(1, 5, 4);
 	scene.background = new THREE.TextureLoader().load('./assets/xique-xique.jpg');
-	scene.add(ball3d, ground);
-	scene.add(...light);
+	scene.add(ball3d, ground, light);
 	scene.add(...player);
+	scene.add(...sprites);
+
+	window.onresize = () => {
+		WINDOW.width = window.innerWidth;
+		WINDOW.height = window.innerHeight;
+		renderer.setSize(WINDOW.width, WINDOW.height);
+		camera.aspect = WINDOW.aspect;
+		camera.updateProjectionMatrix();
+	}
+	BALL_VELOCITY.x = (Math.floor(Math.random() * (2 - 1 + 1) + 1) % 2 ? BALL_VELOCITY.defaultX : -BALL_VELOCITY.defaultX);
+	BALL_VELOCITY.z = (Math.floor(Math.random() * (2 - 1 + 1) + 1) % 2 ? BALL_VELOCITY.defaultZ : -BALL_VELOCITY.defaultZ);
+	sprites[0].scale
 
 	camera.position.set(0, 10, 0);
 	camera.lookAt(scene.position);
+	ball3d.position.z = Math.random() * 10 - 5;
 	function animate() {
 		requestAnimationFrame(animate);
 
-		ballMove(ball3d, player);
+		scoreUpdate(canvas, sprites);
 		playerMove(player);
-		camera.lookAt(scene.position);
+		if (!BALLPAUSE) {
+			ballMove(ball3d, player);
+			ball3d.rotation.x += BALL_VELOCITY.x;
+			ball3d.rotation.z += -BALL_VELOCITY.z;
+		}
 		renderer.render(scene, camera);
 	}
 	animate();
@@ -95,10 +108,10 @@ function setupRenderer(renderer) {
 function setupBall() {
 	const ballTexture = new THREE.TextureLoader().load('./assets/compcube.png');
 	const materialBall = new THREE.MeshStandardMaterial({map: ballTexture});
-	const ballGeometry = new THREE.SphereGeometry(0.3);
+	const ballGeometry = new THREE.SphereGeometry(0.4);
 	const ball = new THREE.Mesh(ballGeometry, materialBall);
 
-	ball.position.y = 0.3;
+	ball.position.y = 0.4;
 	ball.castShadow = true;
 	ball.receiveShadow = true;
 	ball.geometry.computeBoundingSphere();
@@ -108,7 +121,7 @@ function setupBall() {
 function setupGround() {
 	const groundTexture = new THREE.TextureLoader().load('./assets/table.png');
 	const materialGround = new THREE.MeshStandardMaterial({map: groundTexture});
-	const groundGeo = new THREE.PlaneGeometry(10, 10);
+	const groundGeo = new THREE.PlaneGeometry(20, 20);
 	const ground = new THREE.Mesh(groundGeo, materialGround);
 
 	ground.receiveShadow = true;
@@ -117,10 +130,10 @@ function setupGround() {
 	return ground;
 }
 
-function setupLight(x, z, color = 0xffffff) {
-	const light = new THREE.DirectionalLight(color, 1.0);
+function setupLight(color = 0xffffff) {
+	const light = new THREE.DirectionalLight(color, 2.0);
 
-	light.position.set(x, 10, z);
+	light.position.set(0, 100, 0);
 	light.castShadow = true;
 	return light;
 }
@@ -148,18 +161,28 @@ function playerMove(player) {
 function ballMove(ball3d, player) {
 	ball3d.position.x += BALL_VELOCITY.x;
 	ball3d.position.z += BALL_VELOCITY.z;
-	if (ball3d.position.z > 3.9 || ball3d.position.z < -3.9)
-		BALL_VELOCITY.z = -BALL_VELOCITY.z * (Math.abs(BALL_VELOCITY.z) < 1 ? 1.1 : 1);
+	if (Math.abs(ball3d.position.z) > 7.5)
+		BALL_VELOCITY.z = -BALL_VELOCITY.z * (Math.random() * (1.1 - 1.01) + 1.01);
+	else if (Math.abs(ball3d.position.x) > 10) {
+		if (ball3d.position.x < 0)
+			SCORE.playerTwo++;
+		else
+			SCORE.playerOne++;
+		BALL_VELOCITY.x = (Math.random() < 0.5 ? BALL_VELOCITY.defaultX : -BALL_VELOCITY.defaultX);
+		BALL_VELOCITY.z = (Math.random() < 0.5 ? BALL_VELOCITY.defaultZ : -BALL_VELOCITY.defaultZ);
+		ball3d.position.x = 0;
+		ball3d.position.z = Math.random() * 8 - 4;
+		BALLPAUSE = true;
+		setTimeout(() => { BALLPAUSE = false }, 500);
+	}
 	else if (BALL_VELOCITY.timer > 0) {
 		BALL_VELOCITY.timer--;
 		return ;
 	}
 	else if (playerColision(ball3d, player)) {
-		BALL_VELOCITY.x = -BALL_VELOCITY.x * (Math.abs(BALL_VELOCITY.x) < 1 ? 1.1 : 1);
-		BALL_VELOCITY.timer = 500;
+		BALL_VELOCITY.x = -BALL_VELOCITY.x * (Math.random() * (1.1 - 1.01) + 1.01);
+		BALL_VELOCITY.timer = 5 / Math.abs(BALL_VELOCITY.x);
 	}
-	else if (ball3d.position.x > 4 || ball3d.position.x < -4)
-		BALL_VELOCITY.x = -BALL_VELOCITY.x * (Math.abs(BALL_VELOCITY.x) < 1 ? 1.1 : 1);
 }
 
 function playerColision(ball3d, player) {
@@ -169,3 +192,15 @@ function playerColision(ball3d, player) {
 
 	return sphere.intersectsBox(box) || sphere.intersectsBox(box2);
 }
+
+function scoreUpdate(canvas, sprites) {
+	const context = [canvas[0].getContext('2d'), canvas[1].getContext('2d')];
+
+	context[0].clearRect(0, 0, canvas[0].width, canvas[0].height);
+	context[1].clearRect(0, 0, canvas[1].width, canvas[1].height);
+	context[0].fillText(SCORE.playerOne, 0, 256);
+	context[1].fillText(SCORE.playerTwo, 0, 256);
+	sprites[0].material.map.needsUpdate = true;
+	sprites[1].material.map.needsUpdate = true;
+}
+
