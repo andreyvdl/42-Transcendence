c1dcbc73a74823e6ccec6cc1830d6e4841637ee4
adantas- <adantas-@student.42sp.org.br>
2024-05-24 16:31:08 -0300

    feat: added dashboard

--- pages/views.py
+++ pages/views.py
@@ -4,5 +4,13 @@ from random import randint, random
 def index(request):
 	return render(request, "index.html")
 
+def dashboard(request):
+	nomes = [ "Sonic", "Mario", "Cloud", "Link", "Kratos", "Mrs. Croft", "Master Chief", "Samus", "Doomguy", "Pikachu", "Kirby", "Donkey Kong", "Yoshi", "Snake"]
+
+	return render(request, "dashboard.html", {
+		"name": nomes[randint(0, len(nomes)-1)],
+		"ratio": "{:.2f}".format(random()),
+	})
+
 def redirect_to_login(request):
 	return redirect("/login")
