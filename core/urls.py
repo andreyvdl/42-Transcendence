b8691cd69ffe065d01cfd990057a9dd93331b876
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2024-05-27 17:41:14 -0300

    using pages path

--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("register/", include("pages.urls")),
+    path("pages/", include("pages.urls")),
     path("admin/", admin.site.urls),
 ]
