2c702e674576cd35949ee2b941ba625c230c9ffa
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2024-05-27 17:42:20 -0300

    login path

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.RegisterView.as_view(), name="register"),
+    path("register", views.RegisterView.as_view(), name="register"),
+    path("login", views.LoginView.as_view(), name="login")
 ]
