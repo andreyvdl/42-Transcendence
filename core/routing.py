2d6e52831bc48ba2c8be5864d0539e55538da7d5
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2024-05-28 09:19:36 -0300

    feat: added new consumer returning to the requested page

--- core/routing.py
+++ core/routing.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from core.consumers import DefaultConsumer
+from pages.consumers import ConsumerPage
 
 websocket_urlpatterns = [
     path('wss/', DefaultConsumer.as_asgi()),
+    path('wss/page', ConsumerPage.as_asgi()),
 ]
