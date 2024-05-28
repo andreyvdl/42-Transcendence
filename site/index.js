fd488fea557e057339717f960d09db0da63672ef
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2024-05-28 09:21:42 -0300

    feat: added simple SPA configuration with routes home, login and register with websockets

--- site/index.js
+++ site/index.js
@@ -1,30 +1,19 @@
-window.addEventListener('hashchange', () => {
-	console.log('hashchange', location.hash);
-});
-
 window.addEventListener('DOMContentLoaded', () => {
-	const socket = new WebSocket('wss://localhost:5000/wss/');
+	var socket = new WebSocket("wss://localhost:5000/wss/page");
 
-	socket.onopen = function (event) {
-		console.log('Connected to Login WebSocket Server');
+	var path = window.location.pathname;
+
+	socket.onopen = function() {
+		if (path === "/") {
+			socket.send(JSON.stringify({ 'type' : 'home' }));
+		} else if (path === "/login") {
+			socket.send(JSON.stringify({ 'type' : 'login' }));
+		} else if (path === "/register") {
+			socket.send(JSON.stringify({ 'type' : 'register' }));
+		}
 	}
 
-	socket.onmessage = function (event) {
-		console.log("Message sent by the server:")
-		console.log(JSON.parse(event.data))
+	socket.onmessage = function(event) {
+		document.getElementById("mainContent").innerHTML = event.data;
 	}
-
-	socket.onclose = function (event) {
-		console.log('Disconnected from WebSocket server');
-	}
-
-	document.querySelector('#formContainer').addEventListener('submit', (event) => {
-		event.preventDefault();
-		const email = document.getElementById('email_input').value
-		const password = document.getElementById('pass_input').value
-		socket.send(JSON.stringify({
-			'email': email,
-			'password': password,
-		}));
-	});
 })
