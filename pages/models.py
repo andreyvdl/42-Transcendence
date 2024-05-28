cdebd6cab1f92be60dc4843f928e9d80e4b2a442
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2024-05-27 23:14:36 -0300

    PongUser model

--- pages/models.py
+++ pages/models.py
@@ -1,1 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+DEFAULT_ICON = "https://ddragon.leagueoflegends.com/cdn/14.1.1/img/profileicon/7.png"
+
+class PongUser(AbstractUser):
+    wins = models.IntegerField(default = 0)
+    losses = models.IntegerField(default = 0)
+    avatar = models.CharField(default = DEFAULT_ICON)
+
+    def get_wins(self):
+        return self.wins
+
+    def get_losses(self):
+        return self.losses
+
+    def get_avatar(self):
+        return self.avatar
