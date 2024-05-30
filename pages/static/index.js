7284987eb6c8b7be698793727384452a945431d7
Fabio Nacarelli <nacarellifabio@hotmail.com>
2024-05-30 18:21:34 -0300

    add some protections in case fields are not available and also implemented logic to add friend

--- pages/static/index.js
+++ pages/static/index.js
@@ -1,23 +1,42 @@
-document.getElementById('accept-btn').addEventListener('click', (event) => {
-    const userToAccept = event.target.className
-    const url = "http://localhost:8000/pages/answer_friend_request/" + userToAccept
+const PREFIX = "http://localhost:8000/"
+const acceptBtn = document.getElementById("accept-btn");
+const declineBtn = document.getElementById("decline-btn");
+const friendToAddTextField = document.getElementById('friend-text-field')
 
-    fetch(url, {
-        method: 'POST',
-        body: JSON.stringify({
-            'ans': 'n'
+if (acceptBtn) {
+    acceptBtn.addEventListener('click', (event) => {
+        const userToAccept = event.target.className
+        const url = "http://localhost:8000/pages/answer_friend_request/" + userToAccept
+
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({
+                'ans': 'y'
+            })
         })
     })
-})
+}
 
-document.getElementById('decline-btn').addEventListener('click', (event) => {
-    const userToAccept = event.target.className
-    const url = "http://localhost:8000/pages/answer_friend_request/" + userToAccept
+if (declineBtn) {
+    document.getElementById('decline-btn').addEventListener('click', (event) => {
+        const userToDecline = event.target.className
+        const url = "http://localhost:8000/pages/answer_friend_request/" + userToDecline
+
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({
+                'ans': 'n'
+            })
+        })
+    })
+}
+
+
+document.getElementById('add-friend-btn').addEventListener('click', (event) => {
+    const friend = friendToAddTextField.value
+    const url = PREFIX + '/pages/make_friends/' + friend + '/'
 
     fetch(url, {
-        method: 'POST',
-        body: JSON.stringify({
-            'ans': 'n'
-        })
+        method: 'POST'
     })
 })
