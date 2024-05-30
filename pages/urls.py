367ccf3539aeb5747d607cc8f43f3c35360de22b
Fabio Nacarelli <nacarellifabio@hotmail.com>
2024-05-30 11:59:55 -0300

    fix: login_required on account view

--- pages/urls.py
+++ pages/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
 from . import views
 
+# https://docs.djangoproject.com/en/dev/topics/class-based-views/intro/#decorating-in-urlconf
 urlpatterns = [
     path("register", views.RegisterView.as_view(), name="register"),
     path("login", views.LoginView.as_view(), name="login"),
-    path("account", views.AccountView.as_view(), name="account"),
+    path("account", login_required(views.AccountView.as_view(), login_url='login'), name="account"),
     path("save_match/<int:right_pk>/<str:score>/<int:pk_winner>", views.save_match, name="save_match"),
     path("make_friends/<str:user2>", views.make_friends, name="make_friends")
 ]
