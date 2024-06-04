72ed06354cb3d4dbf4da3973f521844910f74714
Fabio Nacarelli <nacarellifabio@hotmail.com>
2024-06-04 19:21:57 -0300

    feat: profile picture field for user

--- pages/models.py
+++ pages/models.py
@@ -2,22 +2,17 @@ from datetime import datetime
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-DEFAULT_ICON = "https://ddragon.leagueoflegends.com/cdn/14.1.1/img/profileicon/7.png"
-
 
 class PongUser(AbstractUser):
     wins = models.IntegerField(default=0)
     losses = models.IntegerField(default=0)
-    avatar = models.CharField(default=DEFAULT_ICON)
+    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
 
     def get_wins(self):
         return self.wins
 
     def get_losses(self):
         return self.losses
-
-    def get_avatar(self):
-        return self.avatar
 
 
 class Match(models.Model):
