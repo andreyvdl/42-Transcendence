ae25ca8ccc68e9432256f5b0576a0de99af6fa95
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2024-06-05 14:11:26 -0300

    refactor: reworked in file structure

--- pages/static/main.js
+++ pages/static/main.js
@@ -2,11 +2,35 @@ window.addEventListener("DOMContentLoaded", () => {
 	handleLocation();
 	window.onpopstate = handleLocation;
 
-    const navLinks = document.querySelectorAll('a.nav-link');
-    navLinks.forEach(link => {
-        link.addEventListener('click', route);
-    });
-})
+    document.querySelectorAll('a.nav-link').forEach(link => {
+		link.addEventListener('click', route);
+	});
+});
+
+function initializePageScripts(url) {
+	if (url == '/pong') {
+		initializePongGame();
+	}
+	else if (url == '/pong-3d') {
+		const { initializePongGame3d } = import('/static/pong-game-3d/index.js');
+		initializePongGame3d();
+	}
+};
+
+const handleLocation = async () => {
+	const route = window.location.pathname;
+	await fetch(route, {
+		headers : {
+			'Accept': 'text/html',
+			'X-Requested-With': 'XMLHttpRequest',
+		},
+	})
+	.then(Response => Response.text())
+	.then(html => {
+		document.getElementById("mainContent").innerHTML = html;
+		initializePageScripts(route);
+	});
+};
 
 const route = (event) => {
     event.preventDefault();
@@ -17,17 +41,4 @@ const route = (event) => {
 	if (targetUrl != currentUrl)
 		window.history.pushState({}, "", event.target.href);
     handleLocation();
-};
-
-const handleLocation = async () => {
-	const route = window.location.pathname;
-	const html = await fetch(route, {
-		headers:{
-			'Accept': 'application/json',
-			'X-Requested-With': 'XMLHttpRequest',
-		},
-	})
-	.then((data) => data.text());
-	console.log(html);
-	document.getElementById("mainContent").innerHTML = html;
 };
