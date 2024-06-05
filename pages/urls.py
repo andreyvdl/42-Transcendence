b29b289412a44425b23274c70e0f0ce8fa1cf70f
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2024-06-05 17:55:42 -0300

    refactor: temporarily removing games

--- pages/urls.py
+++ pages/urls.py
@@ -6,7 +6,4 @@ urlpatterns = [
 	path("login", views.Login.as_view(), name="login"),
 	path("register", views.Register.as_view(), name="register"),
 	path("dashboard", views.dashboard, name="dashboard"),
-	path("pong", views.pong, name="pong"),
-	path("pong-3d", views.pong3D, name="pong3D"),
-	path("jan-ken-pon", views.jkp, name="jan-ken-pon"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -36,24 +36,3 @@ def dashboard(request):
 		"name": nomes[randint(0, len(nomes)-1)],
 		"ratio": "{:.2f}".format(random()),
 	})
-
-def redirect_to_login(request):
-	if not is_ajax(request):
-		return render(request, "base.html")
-	return redirect("/login")
-
-def pong(request):
-	print(request.path)
-	if not is_ajax(request):
-		return render(request, "base.html")
-	return render(request, "pong-game/index.html")
-
-def pong3D(request):
-	if not is_ajax(request):
-		return render(request, "base.html")
-	return render(request, "pong-game-3d/index.html")
-
-def jkp(request):
-	if not is_ajax(request):
-		return render(request, "base.html")
-	return render(request, "jan-ken-pon/index.html")
