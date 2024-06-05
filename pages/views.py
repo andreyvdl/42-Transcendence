110d235d493a4daa3eb7f955de13d7638c29a383
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2024-06-05 14:12:01 -0300

    feat: added new urls from games

--- pages/views.py
+++ pages/views.py
@@ -38,4 +38,22 @@ def dashboard(request):
 	})
 
 def redirect_to_login(request):
+	if not is_ajax(request):
+		return render(request, "base.html")
 	return redirect("/login")
+
+def pong(request):
+	print(request.path)
+	if not is_ajax(request):
+		return render(request, "base.html")
+	return render(request, "pong-game/index.html")
+
+def pong3D(request):
+	if not is_ajax(request):
+		return render(request, "base.html")
+	return render(request, "pong-game-3d/index.html")
+
+def jkp(request):
+	if not is_ajax(request):
+		return render(request, "base.html")
+	return render(request, "jan-ken-pon/index.html")
