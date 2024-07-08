c622e422d41de4b9cc053a4ed4cd1c995e809600
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2024-07-08 19:17:23 -0300

    feat: added new models for matches of tournament

--- tournament/models.py
+++ tournament/models.py
@@ -3,11 +3,22 @@ from datetime import datetime
 from main.models import PongUser
 from django.contrib.postgres.fields import ArrayField
 
+class TournamentMatch(models.Model):
+    match_round = models.IntegerField()
+    player_one = models.CharField(max_length=50)
+    player_two = models.CharField(max_length=50)
+    winner = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return f"{self.player_one} vs {self.player_two}"
+
 # Create your models here.
 class Tournament(models.Model):
+    curr_round = models.IntegerField(default=0)
     players = ArrayField(models.CharField(max_length=50), blank=True)
     winner = models.ForeignKey(PongUser, on_delete=models.CASCADE, related_name="winner", null=True)
     date = models.DateTimeField(auto_now=False, auto_now_add=False, default=datetime.now)
+    round_matches = models.ManyToManyField(TournamentMatch)
 
     def __str__(self):
         return str(self.players)
